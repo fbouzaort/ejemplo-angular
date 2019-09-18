1c83e24d59367cdbdca88dcfd589cb68b3f9217d
docenteFI <docenteFI@facultades.ort.edu.uy>
2019-09-17 21:38:57 -0300

    Creacion app tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,43 @@
 import { Component } from '@angular/core';
+import { Tarea } from "./tarea";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{titulo}}</h1>
+    <h2>Tareas</h2>
+    <div>
+      <ul class="tareas">
+        <li *ngFor="let tarea of tareas" (click)=elegirTarea(tarea)><span>{{tarea.id}}</span> - {{tarea.tituloTarea}}</li>
+      </ul>
+    </div>
+    <mi-tarea-detalle [tareaElegidaDetalle]=tareaElegida></mi-tarea-detalle>
+  `,styles:[`
+    .tareas {
+      list-style-type:none;
+    }
+  `] 
+  
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+  titulo = 'Lista de Tareas';
+  tareas = tareasLista;
+  tareaElegida:Tarea;
+
+  elegirTarea(_tarea:Tarea):void{
+    this.tareaElegida = _tarea;
+  }
+}
+
+
+
+const tareasLista:Tarea[] = [
+  {id:1, tituloTarea:'Primera tarea'},
+  {id:2, tituloTarea:'Segunda tarea'},
+  {id:3, tituloTarea:'Tercera tarea'},
+  {id:4, tituloTarea:'Cuarta tarea'},
+  {id:5, tituloTarea:'Quinta tarea'},
+  {id:7, tituloTarea:'Septima tarea'}
+]
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { FormsModule } from "@angular/forms";
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { TareaDetalleComponent } from "./tarea-detalle.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, TareaDetalleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
