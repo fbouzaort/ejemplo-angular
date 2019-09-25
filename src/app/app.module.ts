fb1b6a45cd4ae188652a600679b5be9fb958c50f
Francisco Bouza <franciscobouza@Franciscos-MacBook-Pro.local>
2019-09-24 21:52:04 -0300

    ngOnInit + res.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from "./app.component";
 import { TareaDetalleComponent } from "./tarea-detalle.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent, TareaDetalleComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpModule],
+  declarations: [AppComponent, TareaDetalleComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
